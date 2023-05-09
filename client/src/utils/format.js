2856e4f30ef5a37a913591d217bea04a1fd5a192
Mohammad Nabiei <z5308349@ad.unsw.edu.au>
2023-05-09 15:27:08 +1000

    Deleted everything

--- client/src/utils/format.js
+++
@@ -1,8 +0,0 @@
-export const formatPrice = (price) => {
-  return `$${price.toFixed(2)}`;
-};
-
-export const formatDate = (dateString) => {
-  const date = new Date(dateString);
-  return date.toLocaleDateString();
-};
